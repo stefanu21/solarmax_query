58aacd2c7365cc6210c4e6dea5c8d4aaccf3fd7b
Stefan Ursella <stefan.ursella@wolfvision.net>
2023-02-10 19:24:56 +0100

    myPVlogger.py: add dry-run option

--- myPVlogger.py
+++ myPVlogger.py
@@ -3,11 +3,14 @@
 from solarmax_query import SolarMax
 import rrdtool
 import os
+from random import *
+
 
 class MyPVLogger(SolarMax):
-    def __init__(self, host, port, db_name):
+    def __init__(self, host, port, db_name, dry_run=False):
         self.db_name = db_name
-        super().__init__(host, port, 0)
+        self.dry_run = dry_run
+
         if not os.path.exists(db_name):
             rrdtool.create(db_name, "--start", "now",
                         "--step", "55",
@@ -18,6 +21,20 @@ class MyPVLogger(SolarMax):
                         "RRA:AVERAGE:0.5:1:8640",
                         "RRA:AVERAGE:0.5:6:6048",
                         "RRA:AVERAGE:0.5:60:4464")
+        try:
+            super().__init__(host, port, 0)
+        except Exception as e:
+            print(f' {e}')
+            if not self.dry_run:
+                exit(1)
+
+    def push_dummy(self):
+        data = "N:" + str(random()) + ":" + \
+            str(randint(1,30)) + ":"  \
+            + str(randint(5,60)) + ":" \
+            + str(randint(7,90))
+        print(f'add {data}')
+        rrdtool.update(self.db_name, data)
 
     def push_data(self):
         data = "N:" + str(self.acOutput()) + ":" + \
@@ -29,8 +46,12 @@ class MyPVLogger(SolarMax):
 
 
 def main():
-    s = MyPVLogger('192.168.40.210', 12345, "data.rrd")
-    s.push_data()
+    s = MyPVLogger('192.168.40.210', 12345,
+                   "/home/stefan/backup/playground/solarmax_query/data.rrd")
+    if s.dry_run:
+        s.push_dummy()
+    else:
+        s.push_data()
 
 
 if __name__ == "__main__":
